9ef157073dfd48fc02270161ab3c2228537b3268
liza <lizaessien23@gmail.com>
2020-06-17 20:54:08 +0300

    first commit

---
+++ scripts/ui.js
@@ -0,0 +1,21 @@
+class ChatUI{
+    constructor(list){
+        this.list=list;
+    }
+
+    clear(){
+        this.list.innerHTML='';
+    }
+    render(data){
+        const when=dateFns.distanceInWordsToNow(
+            data.created_at.toDate(),{ addSuffix: true}
+        )
+        const html=`
+        <li>
+        <span class="username">${data.username}:</span>
+        <span>${data.message}</span>
+        <div>${when}</div>
+        <li>`;
+        this.list.innerHTML+=html;
+    }
+}
