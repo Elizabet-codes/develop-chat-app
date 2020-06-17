47891a0feb0edd4da715e2eb5db2b94f2d866faf
liza <lizaessien23@gmail.com>
2020-06-17 21:31:27 +0300

    allowed req from dateFNS

--- scripts/ui.js
+++ scripts/ui.js
@@ -7,9 +7,10 @@ class ChatUI{
         this.list.innerHTML='';
     }
     render(data){
-        const when=dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),{ addSuffix: true}
-        )
+        const when = dateFns.distanceInWordsToNow(
+          data.created_at.toDate(),
+          { addSuffix:true }
+        );
         const html=`
         <li>
         <span class="username">${data.username}:</span>
